1dc52ed62bd452a85ab87f20266f3070812b8fcf
nilaknatha singh deo <nsdeo12@hotmail.com>
2018-05-22 19:50:12 +0530

    typescript general awareness

---
+++ typescript/1.multiply.ts
@@ -0,0 +1,9 @@
+/**
+ * @author Nilakantha Singh Deo
+ * @email nsinghdeo@protonmail.com
+ * @create date 2018-05-22 02:37:33
+ * @modify date 2018-05-22 02:37:33
+ * @desc [https://github.com/nsdeo12]
+*/
+
+
